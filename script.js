a8b1139ef1fe0ca0fc65dd832cc02f584fe81cc3
Niranjana M R <niranjan.mr99@gmail.com>
2021-06-04 12:14:11 +0530

    added random color generation feature

--- script.js
+++ script.js
@@ -1,41 +1,76 @@
 const container = document.querySelector('#container');
+let box;
+
 
 let value = window.prompt('Enter the number of grids: ');
-checkValue(value);
-createBox(value);
 
-function checkValue(userInput){
-    if(isNaN(userInput)){
+checkValue(value);
+colorBoxes();
+
+function checkValue(userInput) {
+    if (isNaN(userInput)) {
         userInput = window.prompt(`${userInput} is not a number. Please enter the number of grids: `);
         checkValue(userInput);
-    }
-    else if(userInput === ''){
+    } else if (userInput === '') {
         userInput = window.prompt('You forgot to enter a number. Please enter the number of grids: ');
         checkValue(userInput);
-    }
-    else{
-        console.log('a number');
+    } else {
+        createBox(value);
     }
 }
 
-function createBox(boxes){
+function createBox(boxes) {
     createColumns(boxes);
     boxes *= boxes;
-    
-    for(let i = 1; i<=boxes;i++){
-        const div = document.createElement('div');
-        div.setAttribute('id', `box${i}`);
-        div.setAttribute('style', ` border: 1px solid black; width: 50px; height: 50px; justify-self: center;`);
-        container.appendChild(div);
+
+    for (let i = 1; i <= boxes; i++) {
+        box = document.createElement('div');
+        box.classList.add('boxes')
+        box.setAttribute('style', ` border: 1px solid black; width: 50px; height: 50px; justify-self: center;`);
+        container.appendChild(box);
     }
 }
 
-function createColumns(cols){
-    for(let i = 1; i<=cols;i++){
+function createColumns(cols) {
+    for (let i = 1; i <= cols; i++) {
         container.setAttribute('style', `display: grid; 
                                          margin: auto;
                                          justify-content: center; 
-                                         grid-template-columns: repeat(${i}, 50px);`
-                                         );
+                                         grid-template-columns: repeat(${i}, 50px);`);
     }
+}
+
+function colorBoxes() {
+    let mouseTargets = document.querySelectorAll('.boxes');
+    let randomColor = '';
+    mouseTargets.forEach(box => {
+        box.addEventListener('mouseenter', (e) => {
+            randomColor = generateColor(randomColor);
+            box.setAttribute('style', `background-color:${randomColor};
+                                       border: 1px solid black;
+                                       width: 50px;
+                                       height: 50px;
+                                       justify-self: center;
+                            `);
+        })
+    });
+}
+
+function generateColor(color){
+    let r, b, g = 0;
+    let a = 1;
+    r = Math.floor((Math.random()*255000)/255);
+    g = Math.floor((Math.random()*255000)/255);
+    b = Math.floor((Math.random()*255000)/255);
+    if(r > 255){
+        r = 255;
+    }
+    if(g>255){
+        g = 255;
+    }
+    if(b>255){
+        b = 255;
+    }
+    color = `rgba(${r}, ${g}, ${b}, ${a})`;
+    return color;
 }
